84f82c1bd98363e02eb95c15140d94ce1c7a85f1
jaeho-jang <jaehojang@nate.com>
2021-04-22 22:44:26 +0900

    Wip P10804

---
+++ baekjoon/P10804.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10804">카드 역배치</a>
+ */
+public class P10804 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+    }
+
+}
