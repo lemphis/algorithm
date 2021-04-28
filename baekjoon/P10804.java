3b7bf52432beaf8942f9b8fc5dda4577e6db850f
jaeho-jang <jaehojang@nate.com>
2021-04-28 16:26:30 +0900

    Add P10804

--- baekjoon/P10804.java
+++ baekjoon/P10804.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
+import java.util.stream.IntStream;
 
 /**
  * @see <a href="https://www.acmicpc.net/problem/10804">카드 역배치</a>
@@ -10,9 +11,32 @@ import java.util.StringTokenizer;
 public class P10804 {
 
     public static void main(String[] args) throws IOException {
+        int[] answer = solution();
+        print(answer);
+    }
+
+    static int[] solution() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
+        int[] cards = IntStream.rangeClosed(1, 20).toArray();
+
+        for (int i = 0; i < 10; ++i) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int from = Integer.parseInt(st.nextToken()) - 1;
+            int to = Integer.parseInt(st.nextToken()) - 1;
+            int newLength = to - from;
+            int[] temp = Arrays.copyOfRange(cards, from, to + 1);
+            for (int j = 0; j <= newLength; ++j) {
+                cards[from + j] = temp[newLength - j];
+            }
+        }
+
+        return cards;
+    }
+
+    static void print(int[] items) {
+        for (int item : items) {
+            System.out.print(item + " ");
+        }
     }
 
 }
