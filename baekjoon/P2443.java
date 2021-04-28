fdae453619fd9ac7714b2271fa78fd1f43d8349d
jaeho-jang <jaehojang@nate.com>
2021-04-28 18:04:50 +0900

    Wip P2443

---
+++ baekjoon/P2443.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2443">별 찍기 - 6</a>
+ * 
+ */
+public class P2443 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int totalLineNum = Integer.parseInt(reader.readLine());
+
+        
+    }
+
+}
