6dadb0e60f4d5287579c2773225ef11d9b476cde
jaeho-jang <jaehojang@nate.com>
2021-04-29 13:53:49 +0900

    Add P2443

--- baekjoon/P2443.java
+++ baekjoon/P2443.java
@@ -12,7 +12,17 @@ public class P2443 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int totalLineNum = Integer.parseInt(reader.readLine());
 
-        
+        for (int i = 0; i < totalLineNum; ++i) {
+            for (int j = 0; j < i; ++j) {
+                System.out.print(" ");
+            }
+
+            int starNum = (totalLineNum * 2) - ((i + 1) * 2 - 1);
+            for (int j = 0; j < starNum; ++j) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 
 }
