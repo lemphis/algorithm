ca1c1b21d0954001b2627504eff7edbb00d95214
jaeho-jang <jaehojang@nate.com>
2021-05-08 15:05:34 +0900

    Wip P13300

---
+++ baekjoon/P13300.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/13300">방 배정</a>
+ */
+public class P13300 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+
+}
