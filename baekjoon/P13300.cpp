76ab5339c20e51ab42b1e7a4e89f5081464b0fcf
jaeho-jang <jaehojang@nate.com>
2021-05-10 17:14:41 +0900

    Add P13300
    * Java
    * C++

---
+++ baekjoon/P13300.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main() {
+    int grades[2][6] = { 0 };
+    int studentNum, max, gender, grade;
+    int minRoomCount = 0;
+
+    cin >> studentNum >> max;
+    for (int i = 0; i < studentNum; ++i) {
+        cin >> gender >> grade;
+        grades[gender][grade - 1]++;
+    }
+
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            minRoomCount += ceil(double(grades[i][j]) / double(max));
+        }
+    }
+
+    cout << minRoomCount;
+}
--- baekjoon/P13300.java
+++ baekjoon/P13300.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * @see <a href="https://www.acmicpc.net/problem/13300">방 배정</a>
@@ -9,7 +10,27 @@ public class P13300 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+
+        int[][] grades = new int[2][6];
+        int studentNum = Integer.parseInt(tokenizer.nextToken());
+        int max = Integer.parseInt(tokenizer.nextToken());
+
+        for (int i = 0; i < studentNum; ++i) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            int gender = Integer.parseInt(tokenizer.nextToken());
+            int grade = Integer.parseInt(tokenizer.nextToken());
+            grades[gender][grade - 1]++;
+        }
+
+        int minRoomCount = 0;
+        for (int i = 0; i < 2; ++i) {
+            for (int j = 0; j < 6; ++j) {
+                minRoomCount += Math.ceil((double)grades[i][j] / (double)max);
+            }
+        }
+
+        System.out.println(minRoomCount);
     }
 
 }
