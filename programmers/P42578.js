e3d69342384ea86357375ff671fa66a3248659fd
jaeho-jang <jaehojang@nate.com>
2021-05-12 12:14:40 +0900

    Add P42578

---
+++ programmers/P42578.js
@@ -0,0 +1,20 @@
+/**
+ * @see [위장]{@link https://programmers.co.kr/learn/courses/30/lessons/42578}
+ */
+function solution(clothes) {
+    const kindObj = {};
+    clothes.forEach(element => {
+        const kind = element[1];
+        if (kindObj.hasOwnProperty(kind)) {
+            kindObj[kind]++;
+        } else {
+            kindObj[kind] = 1;
+        }
+    });
+
+    const answer = Object.values(kindObj)
+        .map(val => (val += 1))
+        .reduce((acc, cur) => acc * cur, 1);
+
+    return answer - 1;
+}
