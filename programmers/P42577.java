5a346dbd7bde352c0d932f301aae64eb84b669b6
jaeho-jang <jaehojang@nate.com>
2021-05-12 18:06:40 +0900

    Wip P42577

---
+++ programmers/P42577.java
@@ -0,0 +1,9 @@
+/**
+ * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/42577">전화번호 목록</a>
+ */
+class P42577 {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
+    }
+}
