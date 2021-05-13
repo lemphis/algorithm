7920c8d14d3fa4b99743c219f48576304d547784
jaeho-jang <jaehojang@nate.com>
2021-05-13 17:17:40 +0900

    Add P42577

--- programmers/P42577.java
+++ programmers/P42577.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 /**
  * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/42577">전화번호 목록</a>
  */
 class P42577 {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+
+    public static boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+        int limit = phone_book.length - 1;
+        for (int i = 0; i < limit; ++i) {
+            if (phone_book[i + 1].startsWith(phone_book[i]))
+                return false;
+        }
+        return true;
     }
+
 }
