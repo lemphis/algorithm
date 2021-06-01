796e06f849c98aa412ada59ea9dc9d8909920fd3
jaeho-jang <jaehojang@nate.com>
2021-06-01 22:28:06 +0900

    Add P42586

--- programmers/P42586.js
+++ programmers/P42586.js
@@ -4,12 +4,20 @@
 function solution(progresses, speeds) {
     let temp = 0;
     let count = 0;
-    const days = progresses
-            .map((progress, idx) => Math.ceil((100 - progress) / speeds[idx]))
-            .reduce((acc, cur) => {
-                // TODO: reducer 작성
-            }, [])
+    const days = progresses.map((progress, idx) => Math.ceil((100 - progress) / speeds[idx]));
+    temp = days[0];
 
-    var answer = [];
+    let length = days.length;
+    let answer = [];
+    for (let i = 0; i < length; ++i) {
+        if (days[i] > temp) {
+            answer.push(count);
+            count = 1;
+            temp = days[i];
+        } else {
+            count++;
+        }
+    }
+    answer.push(count);
     return answer;
 }
