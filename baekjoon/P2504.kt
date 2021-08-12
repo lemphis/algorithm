2d5ab4bace83bef9b893209cefa4842eb8fd01ed
jaeho-jang <jaehojang@nate.com>
2021-08-12 16:08:19 +0900

    Add P2504

--- baekjoon/P2504.kt
+++ baekjoon/P2504.kt
@@ -5,43 +5,70 @@ import java.util.*
  */
 fun main() {
     val input = readLine()!!
-    val stack = Stack<Char>()
-    var sum = 0
-    var temp = 0
-    var lastStackSize = 0
-    var before: Char? = null
-    input.forEach {
-        if (isStartBracket(it)) {
-            if (!isStartBracket(before)) {
-                sum += temp
-                temp = 0
-            }
-            stack.push(it)
+    val stack = Stack<String>()
+    var errorCode = 0
+    for (it in input) {
+        val str = it.toString()
+        if (str == "(" || str == "[") {
+            stack.push(str)
         } else {
-            if (stack.size == 0 || !isPair(stack.peek(), it)) {
-                println(0)
-                return
+            if (stack.size == 0) {
+                errorCode = -1
+                break
             }
-            stack.pop()
-            when (before) {
-                '(', '[' -> {
-                    lastStackSize = stack.size
-                    temp += if (before == '(') 2 else 3
-                }
-                ')', ']' -> {
-                    if (lastStackSize == stack.size) {
-
-                    } else {
-                        temp *= if (before == ')') 2 else 3
-                    }
-                }
+            val numStr = when (str) {
+                ")" -> "2"
+                "]" -> "3"
+                else -> "-1"
+            }
+            if (numStr == "-1") {
+                errorCode = -1
+                break
+            }
+            if ((stack.peek() == "[" && str == "]") || (stack.peek() == "(" && str == ")")) {
+                stack.pop()
+                stack.push(numStr)
+            } else if ((stack.peek() == "(" && str == "]") || (stack.peek() == "[" && str == ")")) {
+                errorCode = -1
+            } else {
+                errorCode = getNum(stack, numStr, if (str == ")") "(" else "[")
+            }
+            if (errorCode == -1) {
+                break
             }
         }
-        println(temp)
-        before = it
     }
-    println(sum + temp)
+    if (errorCode == -1
+        || stack.contains("[") || stack.contains("(")
+        || stack.contains("]") || stack.contains(")")
+    ) {
+        println(0)
+        return
+    }
+    print(stack.reduce { acc, s -> (acc.toInt() + s.toInt()).toString() }.toInt())
 }
 
-fun isStartBracket(char: Char?): Boolean = char == '(' || char == '['
-fun isPair(start: Char, end: Char): Boolean = (start == '(' && end == ')') || (start == '[' && end == ']')
+fun getNum(stack: Stack<String>, numStr: String, findTarget: String): Int {
+    var errorCode = 0
+    try {
+        if (stack.find { it == findTarget }.isNullOrEmpty()) {
+            errorCode = -1
+        }
+        var acc = stack.pop()
+        while (stack.peek() != findTarget) {
+            if (stack.isEmpty()) {
+                break
+            }
+            val pop = stack.pop()
+            acc = (pop.toInt() + acc.toInt()).toString()
+        }
+        acc *= numStr
+        stack.pop()
+        stack.push(acc)
+    } catch (e: Exception) {
+        errorCode = -1
+    }
+    return errorCode
+}
+
+operator fun String.times(other: String): String = (this.toInt() * other.toInt()).toString()
