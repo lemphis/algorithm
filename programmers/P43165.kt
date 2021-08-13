6e2f754f9e17c7d751832a005e2fa9f534d3295e
jaeho-jang <jaehojang@nate.com>
2021-08-13 17:22:09 +0900

    Add P43165

---
+++ programmers/P43165.kt
@@ -0,0 +1,22 @@
+/**
+ * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/43165">타겟 넘버</a>
+ */
+class Solution {
+    fun solution(numbers: IntArray, target: Int): Int {
+        var answer = 0
+        val size = numbers.size
+        fun dfs(idx: Int, sum: Int) {
+            if (idx == size) {
+                if (sum == target) {
+                    answer++
+                }
+                return
+            }
+            dfs(idx + 1, sum + numbers[idx])
+            dfs(idx + 1, sum - numbers[idx])
+        }
+        dfs(0, 0)
+        println(answer)
+        return answer
+    }
+}
