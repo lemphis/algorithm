8d519082670ea133e9aa77d584fd04efbc7d31d5
jaeho-jang <jaehojang@nate.com>
2021-08-23 17:14:25 +0900

    Add P1009

---
+++ baekjoon/P2920.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::ios;
+using std::string;
+
+/**
+ * @see [음계](https://www.acmicpc.net/problem/2920)
+ */
+int main()
+{
+    cin.tie(NULL), ios::sync_with_stdio(false);
+    int arr[8];
+    int count = 0;
+    string answer;
+    for (int i = 0; i < 8; ++i)
+        cin >> arr[i];
+    for (int i = 0; i < 8; ++i)
+    {
+        if (i + 1 == arr[i])
+            count++;
+        else if (i + 1 == arr[7 - i])
+            count--;
+    }
+
+    if (count == 8)
+        answer = "ascending";
+    else if (count == -8)
+        answer = "descending";
+    else
+        answer = "mixed";
+
+    cout << answer << '\n';
+}
