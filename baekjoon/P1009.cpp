62f24698545e835efc9566d5ab64460aaf3c8a8a
jaeho-jang <jaehojang@nate.com>
2021-08-23 16:31:49 +0900

    Add P1009

---
+++ baekjoon/P1009.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+/**
+ * @see [분산처리](https://www.acmicpc.net/problem/1009)
+ */
+int main()
+{
+    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    int T;
+    cin >> T;
+    while (T--)
+    {
+        int base, exponent;
+        cin >> base >> exponent;
+        if (base > 10) base %= 10;
+        exponent = exponent % 4 + 4;
+        int result = (long)pow(base, exponent) % 10;
+        if (result == 0) result = 10;
+        cout << result << '\n';
+    }
+}
