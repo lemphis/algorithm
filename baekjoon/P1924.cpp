a29b8af1e9533f82fded9a4fc508af4d1279ca12
jaeho-jang <jaehojang@nate.com>
2021-08-23 17:35:59 +0900

    Add P1924

---
+++ baekjoon/P1924.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::string;
+
+/**
+ * @see [2007년](https://www.acmicpc.net/problem/1924)
+ */
+int main()
+{
+    int days[] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int m, d;
+    cin >> m >> d;
+    m -= 1;
+    while (m--)
+    {
+        d += days[m];
+    }
+
+    string answer;
+    switch (d % 7)
+    {
+    case 0:
+        answer = "SUN";
+        break;
+    case 1:
+        answer = "MON";
+        break;
+    case 2:
+        answer = "TUE";
+        break;
+    case 3:
+        answer = "WED";
+        break;
+    case 4:
+        answer = "THU";
+        break;
+    case 5:
+        answer = "FRI";
+        break;
+    case 6:
+        answer = "SAT";
+        break;
+    }
+
+    cout << answer << '\n';
+}
