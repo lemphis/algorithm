dfe28dcedfd4b282e8394e329d5f6ca853946b30
jaeho-jang <jaehojang@nate.com>
2021-08-25 17:23:35 +0900

    Add P2609

---
+++ baekjoon/P2609.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#define endl '\n'
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+int gcd(int a, int b);
+
+/**
+ * @see [최대공약수와 최소공배수](https://www.acmicpc.net/problem/2609)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int a, b;
+    cin >> a >> b;
+    if (a < b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    int GCD = gcd(a, b);
+    cout << gcd(a, b) << endl << a * b / GCD << endl;
+}
+
+int gcd(int a, int b) { return a % b == 0 ? b : gcd(b, a % b); }
