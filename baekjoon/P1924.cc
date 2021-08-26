37aa99c060ca35085b5f619a948d8ee3df5dac42
jaeho-jang <jaehojang@nate.com>
2021-08-26 09:35:01 +0900

    Improve P1924 performance

--- baekjoon/P1924.cc
+++ baekjoon/P1924.cc
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <iostream>
 
 #define endl '\n'
@@ -5,7 +6,6 @@
 using std::cin;
 using std::cout;
 using std::ios;
-using std::string;
 
 /**
  * @see [2007년](https://www.acmicpc.net/problem/1924)
@@ -20,28 +20,28 @@ int main() {
         d += days[m];
     }
 
-    string answer;
+    char answer[4];
     switch (d % 7) {
         case 0:
-            answer = "SUN";
+            strcpy(answer, "SUN");
             break;
         case 1:
-            answer = "MON";
+            strcpy(answer, "MON");
             break;
         case 2:
-            answer = "TUE";
+            strcpy(answer, "TUE");
             break;
         case 3:
-            answer = "WED";
+            strcpy(answer, "WED");
             break;
         case 4:
-            answer = "THU";
+            strcpy(answer, "THU");
             break;
         case 5:
-            answer = "FRI";
+            strcpy(answer, "FRI");
             break;
         case 6:
-            answer = "SAT";
+            strcpy(answer, "SAT");
             break;
     }
 
