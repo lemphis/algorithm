b8e62dd56bd263e37fa27cb7406653dfe2ee5191
jaeho-jang <jaehojang@nate.com>
2021-08-27 13:48:25 +0900

    Add P11050

--- baekjoon/P11050.cc
+++ baekjoon/P11050.cc
@@ -6,10 +6,19 @@ using std::cin;
 using std::cout;
 using std::ios;
 
+int factorial(int num);
+
 /**
  * @see [이항 계수 1](https://www.acmicpc.net/problem/11050)
  */
 int main() {
     ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-    
+    int n, k;
+    cin >> n >> k;
+    cout << factorial(n) / (factorial(n - k) * factorial(k)) << endl;
+}
+
+int factorial(int num) {
+    if (num == 0 || num == 1) return 1;
+    return num * factorial(num - 1);
 }
