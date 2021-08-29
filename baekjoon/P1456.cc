b2799b2c744efe5a54454cfad42824c5ff2aec78
jaeho-jang <jaehojang@nate.com>
2021-08-29 15:12:38 +0900

    Wip P1456

---
+++ baekjoon/P1456.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define endl '\n'
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+/**
+ * @see [거의 소수](https://www.acmicpc.net/problem/1456)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    
+}
