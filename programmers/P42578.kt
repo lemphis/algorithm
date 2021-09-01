274bbaaa6b8e46115556699122b7052f35c39614
jaeho-jang <jaehojang@nate.com>
2021-09-01 09:39:50 +0900

    Simplify P42578

--- programmers/P42578.kt
+++ programmers/P42578.kt
@@ -2,12 +2,5 @@
  * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/42578">위장</a>
  */
 class Solution {
-    fun solution(clothes: Array<Array<String>>): Int {
-        var answer = 1
-        val flat = clothes.map { it[1] }
-        val map = HashMap<String, Int>()
-        for (it in flat) map[it] = map[it]?.plus(1) ?: 1
-        for (it in map) answer *= (it.value + 1)
-        return answer - 1
-    }
+    fun solution(clothes: Array<Array<String>>): Int = clothes.groupBy { it[1] }.map { it.value.size }.fold(1) { acc, cur -> acc * (cur + 1) } - 1
 }
