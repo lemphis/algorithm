0b82acd7abcd29d6d6835fe6366ca7a3f853f4cd
jaeho-jang <jaehojang@nate.com>
2021-09-03 13:51:11 +0900

    Simplify P2609

--- baekjoon/P2609.cc
+++ baekjoon/P2609.cc
@@ -24,4 +24,6 @@ int main() {
     cout << gcd(a, b) << endl << a * b / GCD << endl;
 }
 
-int gcd(int a, int b) { return a % b == 0 ? b : gcd(b, a % b); }
+int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+}
