6ef4ccc4c37b393976ed2d474b7ac46a711c522f
jaeho-jang <jaehojang@nate.com>
2021-09-06 18:03:24 +0900

    Wip P1790

---
+++ baekjoon/P1790.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#define endl '\n'
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+/**
+ * @see [수 이어 쓰기 2](https://www.acmicpc.net/problem/1790)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int N, k;
+    cin >> N >> k;
+    
+}
