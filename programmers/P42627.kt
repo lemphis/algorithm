05bb4ddcde7c91699cd8615f751abc04c7659ac9
jaeho-jang <jaehojang@nate.com>
2021-09-07 21:08:29 +0900

    Wip P42627

---
+++ programmers/P42627.kt
@@ -0,0 +1,12 @@
+/**
+ * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/42627">디스크 컨트롤러</a>
+ */
+fun solution(jobs: Array<IntArray>): Int {
+    var answer = 0
+    val size = jobs.size
+    val jobList = jobs
+        .sortedWith(compareBy({ it[0] }, { it[1] }))
+        .toMutableList()
+    // TODO:
+    return answer
+}
