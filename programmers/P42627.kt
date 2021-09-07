44e411695a1c104d62c72bd3fd8662b9b9c845d7
jaeho-jang <jaehojang@nate.com>
2021-09-07 21:10:04 +0900

    Add class to P42627

--- programmers/P42627.kt
+++ programmers/P42627.kt
@@ -1,12 +1,13 @@
 /**
  * @see <a href="https://programmers.co.kr/learn/courses/30/lessons/42627">디스크 컨트롤러</a>
  */
-fun solution(jobs: Array<IntArray>): Int {
-    var answer = 0
-    val size = jobs.size
-    val jobList = jobs
-        .sortedWith(compareBy({ it[0] }, { it[1] }))
-        .toMutableList()
-    // TODO:
-    return answer
+class Solution {
+    fun solution(jobs: Array<IntArray>): Int {
+        var answer = 0
+        val size = jobs.size
+        val jobList = jobs
+            .sortedWith(compareBy({ it[0] }, { it[1] }))
+            .toMutableList()
+        return answer
+    }
 }
