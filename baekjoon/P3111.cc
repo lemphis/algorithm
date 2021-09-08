fa9c4ebc5b651c39206a8d10d6afb8b10d3cd5ec
jaeho-jang <jaehojang@nate.com>
2021-09-08 17:50:36 +0900

    Wip P3111

---
+++ baekjoon/P3111.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+
+#define endl '\n'
+
+using std::cin;
+using std::cout;
+using std::ios;
+using std::stack;
+
+/**
+ * @see [검열](https://www.acmicpc.net/problem/3111)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    char A[26], T[300001];
+    cin >> A >> T;
+    stack<char> left, right;
+}
