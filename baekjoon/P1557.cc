1f8a256f5866752d33a554dec49df84ba87f2cfa
jaeho-jang <jaehojang@nate.com>
2021-09-13 18:31:41 +0900

    Wip P1557

---
+++ baekjoon/P1557.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#define endl '\n'
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+/**
+ * @see [제곱 ㄴㄴ](https://www.acmicpc.net/problem/1557)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    
+}
