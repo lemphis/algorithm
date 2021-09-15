0f04f1d9cab799ae998917b01a341090f3a8f269
jaeho-jang <jaehojang@nate.com>
2021-09-16 00:02:21 +0900

    Wip P2493

---
+++ baekjoon/P2493.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+#define endl '\n'
+
+using std::cin;
+using std::cout;
+using std::ios;
+
+/**
+ * @see [탑](https://www.acmicpc.net/problem/2493)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int N;
+    cin >> N;
+    int heights[N];
+    for (int i = 0; i < N; ++i) {
+        cin >> heights[i];
+    }
+}
