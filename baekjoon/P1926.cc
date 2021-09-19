364c102308525a084a3172ad9ce12c24518452b9
jaeho-jang <jaehojang@nate.com>
2021-09-19 16:06:17 +0900

    Modify BFS visiting conditions in P1926

--- baekjoon/P1926.cc
+++ baekjoon/P1926.cc
@@ -58,7 +58,7 @@ int bfs(int row, int col, int n, int m, int dx[], int dy[]) {
         for (int i = 0; i < 4; ++i) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-            if (nx >= 0 && nx <= n && ny >= 0 && ny <= m && paper[nx][ny] == 1) {
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && paper[nx][ny] == 1) {
                 q.push({nx, ny});
                 paper[nx][ny] = 0;
                 sum++;
