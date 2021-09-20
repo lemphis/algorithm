a4a630500a8f6b370e1d6316142181fde65f766a
jaeho-jang <jaehojang@nate.com>
2021-09-20 18:01:30 +0900

    Add P1557

--- baekjoon/P1557.cc
+++ baekjoon/P1557.cc
@@ -1,15 +1,44 @@
 #include <iostream>
 
-#define endl '\n'
+using namespace std;
 
-using std::cin;
-using std::cout;
-using std::ios;
+#define endl '\n'
+#define MAX 1000000
+#define ll long long
+
+ll square_free_count(int n);
+
+int mu[MAX];
 
 /**
  * @see [제곱 ㄴㄴ](https://www.acmicpc.net/problem/1557)
  */
 int main() {
     ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-    
+    mu[1] = 1;
+    for (int i = 1; i < MAX; i++) {
+        for (int j = 2 * i; j < MAX; j += i) {
+            mu[j] -= mu[i];
+        }
+    }
+    ll K;
+    cin >> K;
+    ll start = 0, end = K * 2;
+    while (start < end - 1) {
+        ll mid = (start + end) / 2;
+        if (square_free_count(mid) < K) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    cout << end << endl;
+}
+
+ll square_free_count(int n) {
+    ll count = 0;
+    for (ll i = 1; i * i <= n; ++i) {
+        count += mu[i] * n / (i * i);
+    }
+    return count;
 }
