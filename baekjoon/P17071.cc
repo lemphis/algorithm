aaaa787410ebf1f499042fe6ee5e1d9ef584f1cb
jaeho-jang <jaehojang@nate.com>
2021-09-23 22:26:45 +0900

    Add P17071

--- baekjoon/P17071.cc
+++ baekjoon/P17071.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 int bfs(int n, int k);
 
-bool visit[MAX] = {0};
+bool visit[MAX][2] = {0};
 
 /**
  * @see [숨바꼭질 5](https://www.acmicpc.net/problem/17071)
@@ -17,39 +17,41 @@ int main() {
     ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
     int N, K;
     cin >> N >> K;
-    cout << bfs(N, K) << endl;
+    cout << (N == K ? 0 : bfs(N, K)) << endl;
 }
 
 int bfs(int n, int k) {
     queue<int> q;
-    visit[n] = 1;
+    visit[n][0] = 1;
     q.push(n);
-    int count = 0;
-    int speed = 0;
+    int count = 1;
+    int speed = 1;
 
     while (!q.empty()) {
         int size = q.size();
-        for (int i = 0; i < size; ++i) {
+        while (size--) {
             int now = q.front();
-            if (now == k + speed) {
-                return count;
-            }
             q.pop();
-            if (now - 1 >= 0 && !visit[now - 1]) {
-                visit[now - 1] = 1;
-                q.push(now - 1);
-            }
-            if (now + 1 < MAX && !visit[now + 1]) {
-                visit[now + 1] = 1;
-                q.push(now + 1);
-            }
-            if (now * 2 < MAX && !visit[now * 2]) {
-                visit[now * 2] = 1;
-                q.push(now * 2);
+            for (int i = 0; i < 3; ++i) {
+                int next = now;
+                if (i == 0) {
+                    next += 1;
+                } else if (i == 1) {
+                    next -= 1;
+                } else {
+                    next *= 2;
+                }
+                if (next >= 0 && next < MAX && !visit[next][count % 2]) {
+                    q.push(next);
+                    visit[next][count % 2] = 1;
+                }
             }
         }
         if (k + speed >= MAX) {
             return -1;
+        }
+        if (visit[k + speed][count % 2]) {
+            break;
         }
         count++;
         speed += count;
