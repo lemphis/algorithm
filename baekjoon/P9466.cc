510c325c4f278cc94ae389a1782a3bd33578e1a2
jaeho-jang <jaehojang@nate.com>
2021-09-23 18:53:45 +0900

    Wip P9466

---
+++ baekjoon/P9466.cc
@@ -0,0 +1,24 @@
+#include <cstring>
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
+
+#define endl '\n'
+#define MAX 100001
+
+int students[MAX];
+bool visit[MAX];
+
+/**
+ * @see [텀 프로젝트](https://www.acmicpc.net/problem/9466)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--) {
+        // TODO: code here
+    }
+}
