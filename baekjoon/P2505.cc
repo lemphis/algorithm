261302a3789ff81b5e15c901068fb76322eae0fc
jaeho-jang <jaehojang@nate.com>
2021-10-05 17:18:55 +0900

    Add P2505

--- baekjoon/P2505.cc
+++ baekjoon/P2505.cc
@@ -1,74 +1,99 @@
 #include <algorithm>
-#include <iostream>
-#include <vector>
+#include <cstdio>
 
-#define endl '\n'
+using namespace std;
 
-using std::cin;
-using std::cout;
-using std::find;
-using std::ios;
-using std::is_sorted;
-using std::reverse;
-using std::vector;
-
-void print(int times, int* front, int* back);
+void find_front(int cards[], int cards_size, int front[], int back[], int& times);
+void find_back(int cards[], int cards_size, int front[], int back[], int& times);
+void print(int times, int front[], int back[]);
 
 /**
  * @see [두 번 뒤집기](https://www.acmicpc.net/problem/2505)
  */
 int main() {
-    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-    int n, cnt = 0, times = 0;
-    int front[2] = {0}, back[2] = {0};
-    cin >> n;
-    vector<int> nums(n + 1);
-    vector<int>::iterator p_find;
-    for (int i = 1; i <= n; ++i) cin >> nums[i];
-    vector<int> temp = nums;
-    while (cnt < 2) {
-        for (int i = 1; i <= n; ++i) {
-            if (i != temp[i]) {
-                p_find = find(temp.begin() + 1, temp.end(), i);
-                reverse(temp.begin() + i, p_find + 1);
-                front[cnt] = i;
-                back[cnt] = p_find - temp.begin();
-                times++;
-                break;
-            }
-        }
-        cnt++;
+    int N;
+    scanf("%d", &N);
+    int cards[N + 1], temp[N + 1];
+    for (int i = 1; i <= N; ++i) {
+        int card;
+        scanf("%d", &card);
+        cards[i] = temp[i] = card;
     }
-    if (!is_sorted(temp.begin() + 1, temp.end())) {
-        temp = nums;
-        cnt = 0;
+    int times = 0;
+    int front[2], back[2];
+    for (int i = 0; i < 2; ++i) {
+        find_front(temp, N, front, back, times);
+    }
+    if (!is_sorted(temp + 1, temp + N + 1)) {
         times = 0;
-        while (cnt < 2) {
-            for (int i = n; i > 0; --i) {
-                if (i != temp[i]) {
-                    p_find = find(temp.begin() + 1, temp.end(), i);
-                    reverse(p_find, temp.begin() + i + 1);
-                    front[cnt] = (temp.begin() + i) - p_find;
-                    back[cnt] = i;
-                    times++;
-                    break;
-                }
-            }
-            cnt++;
+        for (int i = 0; i < 2; ++i) {
+            front[i] = back[i] = 0;
+            find_back(cards, N, front, back, times);
         }
     }
     print(times, front, back);
+    return 0;
 }
 
-void print(int times, int* front, int* back) {
+void find_front(int cards[], int cards_size, int front[], int back[], int& times) {
+    if (is_sorted(cards + 1, cards + cards_size + 1)) {
+        return;
+    }
+    int temp = times;
+    int idx = 1;
+    find_if(cards + 1, cards + cards_size + 1, [front, temp, &idx](int i) -> bool {
+        if (idx != i) {
+            front[temp] = idx;
+            return true;
+        } else {
+            idx++;
+            return false;
+        }
+    });
+    int second_idx = idx + 1;
+    find_if(cards + 1 + idx, cards + cards_size + 1, [back, temp, idx, &second_idx](int i) -> bool {
+        if (i == idx) {
+            back[temp] = second_idx;
+            return true;
+        } else {
+            second_idx++;
+            return false;
+        }
+    });
+    reverse(cards + front[temp], cards + back[temp] + 1);
+    times++;
+}
+
+void find_back(int cards[], int cards_size, int front[], int back[], int& times) {
+    if (is_sorted(cards + 1, cards + cards_size + 1)) {
+        return;
+    }
+    int temp = times;
+    for (int i = cards_size; i > 0; --i) {
+        if (cards[i] != i) {
+            back[temp] = i;
+            break;
+        }
+    }
+    for (int i = cards_size - (cards_size - back[temp]); i > 0; --i) {
+        if (cards[i] == back[temp]) {
+            front[temp] = i;
+            break;
+        }
+    }
+    reverse(cards + front[temp], cards + back[temp] + 1);
+    times++;
+}
+
+void print(int times, int front[], int back[]) {
     if (times == 0) {
-        cout << 1 << " " << 1 << endl;
-        cout << 1 << " " << 1 << endl;
+        printf("1 1\n");
+        printf("1 1\n");
     } else if (times == 1) {
-        cout << front[0] << ' ' << back[0] << endl;
-        cout << 1 << " " << 1 << endl;
+        printf("%d %d\n", front[0], back[0]);
+        printf("1 1\n");
     } else {
-        cout << front[0] << ' ' << back[0] << endl;
-        cout << front[1] << ' ' << back[1] << endl;
+        printf("%d %d\n", front[0], back[0]);
+        printf("%d %d\n", front[1], back[1]);
     }
 }
