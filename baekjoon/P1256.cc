0fd2f3b26db2a4229a4c351c34bc223bb7a6613c
jaeho-jang <jaehojang@nate.com>
2021-10-10 10:33:40 +0900

    Wip P1256

---
+++ baekjoon/P1256.cc
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+using namespace std;
+
+/**
+ * @see [사전](https://www.acmicpc.net/problem/1256)
+ */
+int main() {
+    int N, M, K;
+    scanf("%d %d %d", &N, &M, &K);
+    int memo[101][101] = {0};
+
+    return 0;
+}
