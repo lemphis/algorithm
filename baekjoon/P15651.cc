274b21a916dc7031df86f0f5ab1839d4e11a1128
jaeho-jang <jaehojang@nate.com>
2021-10-15 15:34:48 +0900

    Add P15651

---
+++ baekjoon/P15651.cc
@@ -0,0 +1,32 @@
+#include <cstdio>
+
+using namespace std;
+
+void dfs(int N, int M, int idx, int depth, int arr[]);
+
+/**
+ * @see [N과 M(3)](https://www.acmicpc.net/problem/15651)
+ */
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    int arr[M];
+    bool visit[M];
+    dfs(N, M, 0, 0, arr);
+    return 0;
+}
+
+void dfs(int N, int M, int idx, int depth, int arr[]) {
+    if (M == depth) {
+        for (int i = 0; i < M; ++i) {
+            printf("%d ", arr[i]);
+        }
+        puts("");
+        return;
+    }
+
+    for (int i = 0; i < N; ++i) {
+        arr[depth] = i + 1;
+        dfs(N, M, idx, depth + 1, arr);
+    }
+}
