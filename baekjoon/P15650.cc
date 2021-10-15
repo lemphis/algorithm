b960a0f456152ed9b88bdc57a5fed89f4bb4496f
jaeho-jang <jaehojang@nate.com>
2021-10-15 15:34:43 +0900

    Add P15650

--- baekjoon/P15650.cc
+++ baekjoon/P15650.cc
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void dfs(int N, int M, int depth, int arr[], bool visit[]);
+void dfs(int N, int M, int idx, int depth, int arr[], bool visit[]);
 
 /**
  * @see [N과 M(2)](https://www.acmicpc.net/problem/15650)
@@ -14,11 +14,11 @@ int main() {
     int arr[M];
     bool visit[M];
     memset(visit, 0, sizeof(visit));
-    dfs(N, M, 0, arr, visit);
+    dfs(N, M, 0, 0, arr, visit);
     return 0;
 }
 
-void dfs(int N, int M, int depth, int arr[], bool visit[]) {
+void dfs(int N, int M, int idx, int depth, int arr[], bool visit[]) {
     if (M == depth) {
         for (int i = 0; i < M; ++i) {
             printf("%d ", arr[i]);
@@ -26,11 +26,12 @@ void dfs(int N, int M, int depth, int arr[], bool visit[]) {
         puts("");
         return;
     }
-    for (int i = 0; i < N; ++i) {
+
+    for (int i = idx; i < N; ++i) {
         if (!visit[i]) {
             visit[i] = true;
-            arr[i] = i + 1;
-            dfs(N, M, depth + 1, arr, visit);
+            arr[depth] = i + 1;
+            dfs(N, M, i, depth + 1, arr, visit);
             visit[i] = false;
         }
     }
