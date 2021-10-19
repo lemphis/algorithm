f953664f4a61f27b9c895bed56069c5476ef65bc
jaeho-jang <jaehojang@nate.com>
2021-10-19 10:05:34 +0900

    Add P1890

--- baekjoon/P1890.cc
+++ baekjoon/P1890.cc
@@ -1,6 +1,9 @@
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
+
+typedef long long ll;
 
 /**
  * @see [점프](https://www.acmicpc.net/problem/1890)
@@ -14,12 +17,18 @@ int main() {
             scanf("%d", &board[r][c]);
         }
     }
-    int memo[N][N];
+    ll memo[N][N];
+    memset(memo, 0, sizeof(memo));
     memo[0][0] = 1;
     for (int r = 0; r < N; ++r) {
         for (int c = 0; c < N; ++c) {
-            // TODO: dp code 작성
+            if (r == N - 1 && c == N - 1) break;
+            if (memo[r][c] > 0) {
+                if (r + board[r][c] < N) memo[r + board[r][c]][c] += memo[r][c];
+                if (c + board[r][c] < N) memo[r][c + board[r][c]] += memo[r][c];
+            }
         }
     }
+    printf("%lld\n", memo[N - 1][N - 1]);
     return 0;
 }
