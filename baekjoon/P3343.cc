9ce160b31256465ceefe0b1433785d2d04eac518
jaeho-jang <jaehojang@nate.com>
2021-11-18 01:04:19 +0900

    Add P3343

--- baekjoon/P3343.cc
+++ baekjoon/P3343.cc
@@ -3,41 +3,34 @@
 
 #define endl '\n'
 
-using std::cin;
-using std::cout;
-using std::ios;
-using std::string;
-using std::swap;
-using std::to_string;
+using namespace std;
+using ll = long long;
 
 /**
  * @see [장미](https://www.acmicpc.net/problem/3343)
  */
 int main() {
     ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
-    long long N;
-    int A, B, C, D;
+    ll N, A, B, C, D;
     cin >> N >> A >> B >> C >> D;
-    if (A < C) {
+
+    if (B * C > A * D) {
         swap(A, C);
         swap(B, D);
     }
-    int a_max = N / A;
-    if (N % A != 0) a_max++;
-    int c_max = N / C;
-    if (N % C != 0) c_max++;
-    string min_price = "99999999999999999999";
-    // TODO: big num 처리, for 반복 횟수 감소 처리
-    for (int i = a_max; i >= 0; --i) {
-        for (int j = 0; j <= c_max; ++j) {
-            if (A * i + C * j >= N) {
-                string price = to_string(i * B + j * D);
-                if (min_price > price) {
-                    min_price = price;
-                }
-                break;
-            }
-        }
+
+    ll price = A * D;
+    ll min_price = 0xffffffffffffffff >> 1;
+    ll c_count = A;
+    while (--c_count >= 0) {
+        ll a_count = (N - (c_count * C)) / A;
+        if (a_count < 0) continue;
+        if ((N - (c_count * C)) % A > 0) ++a_count;
+        price = a_count * B + c_count * D;
+        min_price = min(min_price, price);
     }
+
     cout << min_price << endl;
+
+    return 0;
 }
