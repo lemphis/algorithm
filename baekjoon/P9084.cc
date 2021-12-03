653da581f174349a75cf9f986fc31dd2417a588e
jaeho-jang <jaehojang@nate.com>
2021-12-03 11:51:30 +0900

    Add P9084

---
+++ baekjoon/P9084.cc
@@ -0,0 +1,37 @@
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+/**
+ * @see [동전](https://www.acmicpc.net/problem/9084)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int T;
+    cin >> T;
+    while (T--) {
+        int N;
+        cin >> N;
+        int coin[N];
+        for (int i = 0; i < N; ++i) {
+            cin >> coin[i];
+        }
+        int M;
+        cin >> M;
+        int memo[M + 1];
+        memset(memo, 0, sizeof(memo));
+        memo[0] = 1;
+        for (int i = 0; i < N; ++i) {
+            for (int j = coin[i]; j <= M; ++j) {
+                memo[j] += memo[j - coin[i]];
+            }
+        }
+
+        cout << memo[M] << endl;
+    }
+
+    return 0;
+}
