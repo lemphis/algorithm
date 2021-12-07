5f6e5e081acc12e81210cccea1cd68fba89e2842
jaeho-jang <jaehojang@nate.com>
2021-12-07 15:16:40 +0900

    Add P42587

--- programmers/P42587.cc
+++ programmers/P42587.cc
@@ -5,31 +5,31 @@
 
 using namespace std;
 
-#define endl '\n'
-
 /**
  * @see [프린터](https://programmers.co.kr/learn/courses/30/lessons/42587)
  */
 int solution(vector<int> priorities, int location) {
-    priority_queue<pair<int, int>> pq;
+    queue<pair<int, int>> q;
+    priority_queue<int> pq;
     for (int i = 0; i < priorities.size(); ++i) {
-        pq.push({priorities[i], i});
+        q.push({priorities[i], i});
+        pq.push(priorities[i]);
     }
 
-    int answer = 1;
-    for (int i = 0; i < priorities.size(); ++i) {
-        pair<int, int> item = pq.top();
-        if (item.second == location) break;
-        pq.pop();
-        answer++;
+    int answer = 0;
+    while (!q.empty()) {
+        pair<int, int> item = q.front();
+        q.pop();
+        if (pq.top() == item.first) {
+            pq.pop();
+            answer++;
+            if (item.second == location) {
+                break;
+            }
+        } else {
+            q.push(item);
+        }
     }
 
     return answer;
-}
-
-int main() {
-    vector<int> priorities = {1, 1, 9, 1, 1, 1};
-    int location = 0;
-    cout << solution(priorities, location) << endl;
-    return 0;
 }
