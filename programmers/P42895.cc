fd7f9e2f34ff1bdba72a5a178292ec1c05e96c09
jaeho-jang <jaehojang@nate.com>
2021-12-14 20:26:30 +0900

    Wip P42895

---
+++ programmers/P42895.cc
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/**
+ * @see [N으로 표현](https://programmers.co.kr/learn/courses/30/lessons/42895)
+ */
+int solution(int N, int number) {
+    int answer = 0;
+    return answer;
+}
