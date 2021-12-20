f15de972b6a127eb16d69780af57b3a1a5b1bb38
jaeho-jang <jaehojang@nate.com>
2021-12-20 17:52:57 +0900

    Add P11052

---
+++ baekjoon/P11052.cc
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+#define endl '\n'
+
+/**
+ * @see [카드 구매하기](https://www.acmicpc.net/problem/11052)
+ */
+int main() {
+    ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int N;
+    cin >> N;
+    int price[N + 1];
+    for (int i = 1; i <= N; ++i) {
+        cin >> price[i];
+    }
+
+    int memo[N];
+    memo[1] = price[1];
+    for (int i = 2; i <= N; ++i) {
+        memo[i] = price[i];
+        for (int j = 1; j < i; ++j) {
+            memo[i] = max(memo[i], memo[i - j] + price[j]);
+        }
+    }
+
+    cout << memo[N] << endl;
+
+    return 0;
+}
