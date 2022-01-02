4e751676d08e196fa8df7be33cad5bba8ea5f31b
lemphis <lemphis@gmail.com>
2022-01-02 17:11:43 +0900

    Add P1402

---
+++ baekjoon/P1402.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+
+using namespace std;
+
+/**
+ * @see [아무래도이문제는A번난이도인것같다](https://www.acmicpc.net/problem/1402)
+ */
+int main() {
+    fastio;
+    int T;
+    cin >> T;
+    while (T--) {
+        long long A, B;
+        cin >> A >> B;
+        cout << "yes" << endl;
+    }
+
+    return 0;
+}
