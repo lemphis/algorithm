3195d6cccf63d742f18803cc2aa7c713457a3b14
jaeho-jang <jaehojang@nate.com>
2022-02-11 10:18:20 +0900

    Add P2522

--- baekjoon/P2522.cc
+++ baekjoon/P2522.cc
@@ -1,18 +1,28 @@
-#include <cstdio>
+#include <algorithm>
+#include <iostream>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
 
 using namespace std;
 
 /**
- * @see [별 찍기 - 20](https://www.acmicpc.net/problem/10995)
+ * @see [별 찍기 - 12](https://www.acmicpc.net/problem/2522)
  */
 int main() {
-    int N;
-    scanf("%d", &N);
-    for (int r = 0; r < N; ++r) {
-        for (int c = 0; c < N; ++c) {
-            printf(r & 1 ? " *" : "* ");
+    fastio;
+    int n;
+    cin >> n;
+
+    for (int r = 1; r <= n * 2 - 1; ++r) {
+        for (int c = 0; c < abs(r - n); ++c) {
+            cout << ' ';
         }
-        puts("");
+        for (int c = 0; c < n - abs(r - n); ++c) {
+            cout << '*';
+        }
+        cout << endl;
     }
+
     return 0;
 }
