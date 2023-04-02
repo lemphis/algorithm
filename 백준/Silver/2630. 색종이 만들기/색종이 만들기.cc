9b0fdeae496269895f25246b58d0435f3cb03d49
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2023-04-02 15:01:48 +0900

    [Silver II] Title: 색종이 만들기, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.cc
@@ -0,0 +1,59 @@
+#include <iostream>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+#define MAX 128
+
+using namespace std;
+
+bool paper[MAX][MAX];
+
+void dfs(int start_r, int start_c, int& white, int& blue, int N);
+bool check(int start_r, int start_c, int N);
+
+/**
+ * @see [색종이 만들기](https://www.acmicpc.net/problem/2630)
+ */
+int main() {
+    fastio;
+    int N;
+    cin >> N;
+    for (int r = 0; r < N; ++r) {
+        for (int c = 0; c < N; ++c) {
+            cin >> paper[r][c];
+        }
+    }
+
+    int white = 0, blue = 0;
+    dfs(0, 0, white, blue, N);
+
+    cout << white << endl
+         << blue << endl;
+
+    return 0;
+}
+
+void dfs(int start_r, int start_c, int& white, int& blue, int N) {
+    if (check(start_r, start_c, N)) {
+        paper[start_r][start_c] ? blue++ : white++;
+        return;
+    }
+
+    dfs(start_r, start_c, white, blue, N / 2);
+    dfs(start_r, start_c + N / 2, white, blue, N / 2);
+    dfs(start_r + N / 2, start_c, white, blue, N / 2);
+    dfs(start_r + N / 2, start_c + N / 2, white, blue, N / 2);
+}
+
+bool check(int start_r, int start_c, int N) {
+    bool base = paper[start_r][start_c];
+    for (int r = start_r; r < start_r + N; ++r) {
+        for (int c = start_c; c < start_c + N; ++c) {
+            if (paper[r][c] != base) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
