e1ea1d0f8b6c465e844cf58afc4c4dabcf6dad52
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-07-24 13:17:29 +0900

    [Silver I] Title: 부등호, Time: 4 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.rs
@@ -0,0 +1,85 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut input = String::new();
+    let _ = io::stdin().read_to_string(&mut input);
+    let mut it = input.split_ascii_whitespace();
+    let k = it.next().unwrap().parse::<usize>().unwrap();
+    let mut signs = vec![' '; k];
+    for sign in signs.iter_mut() {
+        *sign = it.next().unwrap().chars().next().unwrap();
+    }
+    let mut visited = vec![false; 11];
+    let mut ans = vec![10; k + 1];
+    for i in (0..10).rev() {
+        ans[0] = i;
+        visited[i] = true;
+        max_dfs(&signs, &mut visited, &mut ans, 1);
+        visited[i] = false;
+        if ans[k] != 10 {
+            for num in ans.iter() {
+                print!("{}", num);
+            }
+            println!();
+            break;
+        }
+    }
+    for i in 0..=k {
+        ans[i] = 10;
+        visited[i] = false;
+    }
+    for i in 0..10 {
+        ans[0] = i;
+        visited[i] = true;
+        min_dfs(&signs, &mut visited, &mut ans, 1);
+        visited[i] = false;
+        if ans[k] != 10 {
+            for num in ans.iter() {
+                print!("{}", num);
+            }
+            break;
+        }
+    }
+}
+
+fn max_dfs(signs: &[char], visited: &mut [bool], ans: &mut [usize], idx: usize) {
+    if idx == ans.len() {
+        return;
+    }
+    for i in (0..10).rev() {
+        if !visited[i] {
+            if (signs[idx - 1] == '<' && ans[idx - 1] < i)
+                || (signs[idx - 1] == '>' && ans[idx - 1] > i)
+            {
+                ans[idx] = i;
+                visited[i] = true;
+                max_dfs(signs, visited, ans, idx + 1);
+                visited[i] = false;
+                if ans[ans.len() - 1] != 10 {
+                    break;
+                }
+            }
+        }
+    }
+}
+
+fn min_dfs(signs: &[char], visited: &mut [bool], ans: &mut [usize], idx: usize) {
+    if idx == ans.len() {
+        return;
+    }
+    for i in 0..10 {
+        if !visited[i] {
+            if (signs[idx - 1] == '<' && ans[idx - 1] < i)
+                || (signs[idx - 1] == '>' && ans[idx - 1] > i)
+            {
+                ans[idx] = i;
+                visited[i] = true;
+                min_dfs(signs, visited, ans, idx + 1);
+                visited[i] = false;
+                if ans[ans.len() - 1] != 10 {
+                    break;
+                }
+            }
+        }
+    }
+}
