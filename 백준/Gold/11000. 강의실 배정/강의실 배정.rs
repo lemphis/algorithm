2b93a459e044344072adf74016c81888f7b73f9b
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-07-30 22:58:05 +0900

    [Gold V] Title: 강의실 배정, Time: 28 ms, Memory: 22000 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.rs
@@ -0,0 +1,27 @@
+use std::{
+    cmp::Reverse,
+    collections::BinaryHeap,
+    io::{self, Read},
+};
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+    let mut it = input.split_whitespace().flat_map(str::parse::<usize>);
+    let n = it.next().unwrap();
+    let mut classes = vec![(0, 0); n];
+    for class in classes.iter_mut() {
+        *class = (it.next().unwrap(), it.next().unwrap());
+    }
+    classes.sort();
+    let mut heap: BinaryHeap<Reverse<usize>> = BinaryHeap::new();
+    heap.push(Reverse(classes[0].1));
+    for (s, t) in classes.iter().skip(1) {
+        let Reverse(value) = *heap.peek().unwrap();
+        if value <= *s {
+            heap.pop();
+        }
+        heap.push(Reverse(*t));
+    }
+    print!("{}", heap.len());
+}
