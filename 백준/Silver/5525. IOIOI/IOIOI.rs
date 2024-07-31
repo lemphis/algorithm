15ee58bfcd55bc40615c26881a35939adc88e77d
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-07-31 12:37:03 +0900

    [Silver I] Title: IOIOI, Time: 8 ms, Memory: 14224 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.rs
+++ 백준/Silver/5525. IOIOI/IOIOI.rs
@@ -4,28 +4,24 @@ fn main() {
     let mut input = String::new();
     let _ = io::stdin().read_to_string(&mut input);
     let mut it = input.split_ascii_whitespace();
-    let (n, _, s) = (
+    let (n, m, s) = (
+        it.next().unwrap().parse::<usize>().unwrap(),
         it.next().unwrap().parse::<usize>().unwrap(),
         it.next().unwrap(),
-        it.next().unwrap(),
     );
-    let mut compare_str = String::with_capacity(n);
-    for i in 0..(n * 2 + 1) {
-        compare_str.push(if i & 1 == 0 { 'I' } else { 'O' });
-    }
-    let s_chars: Vec<char> = s.chars().collect();
-    let compare_str_chars: Vec<char> = compare_str.chars().collect();
+    let mut idx = 0;
+    let mut count = 0;
     let mut ans = 0;
-    for i in 0..(s.len() - compare_str_chars.len() + 1) {
-        let mut is_same = true;
-        for j in 0..(n * 2 + 1) {
-            if s_chars[i + j] != compare_str_chars[j] {
-                is_same = false;
-                break;
+    while idx < m - 2 {
+        if &s[idx..idx + 3] == "IOI" {
+            count += 1;
+            idx += 2;
+            if count >= n {
+                ans += 1;
             }
-        }
-        if is_same {
-            ans += 1;
+        } else {
+            count = 0;
+            idx += 1;
         }
     }
 
