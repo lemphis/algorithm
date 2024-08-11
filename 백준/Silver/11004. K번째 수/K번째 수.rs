fd0f5e444037c325e048e2f7749cab7f46a75e72
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-08-11 20:30:10 +0900

    [Silver V] Title: K번째 수, Time: 244 ms, Memory: 98284 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.rs
+++ 백준/Silver/11004. K번째 수/K번째 수.rs
@@ -1,14 +1,12 @@
-use std::io::{self, Read};
+use std::io::{self};
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
+    let input = io::read_to_string(io::stdin()).unwrap();
     let mut it = input.split_whitespace().flat_map(str::parse::<i32>);
     let (n, k) = (it.next().unwrap() as usize, it.next().unwrap() as usize);
     let mut nums = vec![0; n];
     for num in nums.iter_mut() {
         *num = it.next().unwrap();
     }
-    nums.sort();
-    print!("{}", nums[k - 1]);
+    print!("{}", nums.select_nth_unstable(k - 1).1);
 }
