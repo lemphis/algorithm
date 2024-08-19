fb80b05728ecce03f92ee516098ae4d5bfb13d50
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-08-19 21:17:27 +0900

    [Silver V] Title: BABBA, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/9625. BABBA/BABBA.rs
+++ 백준/Silver/9625. BABBA/BABBA.rs
@@ -5,11 +5,10 @@ fn main() {
     io::stdin().read_to_string(&mut input).unwrap();
     let mut it = input.split_whitespace().flat_map(str::parse::<usize>);
     let k = it.next().unwrap();
-    let mut dp = [(0, 0); 46];
-    dp[0] = (1, 0);
-    dp[1] = (0, 1);
-    for i in 2..=k {
-        dp[i] = (dp[i - 2].0 + dp[i - 1].0, dp[i - 2].1 + dp[i - 1].1);
+    let mut a = 0;
+    let mut b = 1;
+    for _ in 1..k {
+        (a, b) = (b, a + b);
     }
-    print!("{} {}", dp[k].0, dp[k].1);
+    print!("{a} {b}");
 }
