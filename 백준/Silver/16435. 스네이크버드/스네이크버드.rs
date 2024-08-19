f4b6f3191edc937c7330f3609f5112baaac50580
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-08-19 21:37:38 +0900

    [Silver V] Title: 스네이크버드, Time: 0 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.rs
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.rs
@@ -1,17 +1,13 @@
-use std::io::{self, Read};
+use std::io;
 
 fn main() {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
-    let mut it = input.split_whitespace().flat_map(str::parse::<usize>);
-    let (n, mut l) = (it.next().unwrap(), it.next().unwrap());
-    let mut heights: Vec<usize> = (0..n).map(|_| it.next().unwrap()).collect();
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_whitespace().flat_map(str::parse::<usize>);
+    let (_, mut l) = (input.next(), input.next().unwrap());
+    let mut heights: Vec<usize> = input.collect();
     heights.sort_unstable();
-    for &height in heights.iter() {
-        if height > l {
-            break;
-        }
-        l += 1;
-    }
+    l = heights
+        .iter()
+        .fold(l, |acc, &cur| if cur > acc { acc } else { acc + 1 });
     print!("{l}");
 }
