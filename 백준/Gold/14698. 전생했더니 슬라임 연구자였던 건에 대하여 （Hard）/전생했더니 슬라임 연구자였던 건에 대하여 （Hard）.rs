fe994f240eccc23ca15f044865242a6e2a3d646a
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-08-21 12:49:49 +0900

    [Gold IV] Title: 전생했더니 슬라임 연구자였던 건에 대하여 (Hard), Time: 56 ms, Memory: 23712 KB -BaekjoonHub

--- 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.rs
+++ 백준/Gold/14698. 전생했더니 슬라임 연구자였던 건에 대하여 （Hard）/전생했더니 슬라임 연구자였던 건에 대하여 （Hard）.rs
@@ -2,28 +2,26 @@ use std::{
     cmp::Reverse,
     collections::BinaryHeap,
     fmt::Write,
-    io::{self, Read},
+    io::{self, stdin},
 };
 
-const DIVISOR: u128 = 1_000_000_007;
+const DIVISOR: u64 = 1_000_000_007;
 
 fn main() {
-    let mut input = String::new();
-    let _ = io::stdin().read_to_string(&mut input);
-    let mut it = input.split_ascii_whitespace().flat_map(str::parse::<u128>);
-    let t = it.next().unwrap();
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<u64>);
+    let t = input.next().unwrap();
     let mut output = String::new();
     for _ in 0..t {
-        let n = it.next().unwrap();
+        let n = input.next().unwrap();
         let mut sum = 1;
-        let mut min_heap: BinaryHeap<Reverse<u128>> =
-            (0..n).map(|_| Reverse(it.next().unwrap())).collect();
+        let mut min_heap: BinaryHeap<_> = (0..n).map(|_| Reverse(input.next().unwrap())).collect();
         while min_heap.len() > 1 {
             let Reverse(first) = min_heap.pop().unwrap();
             let Reverse(second) = min_heap.pop().unwrap();
             let new_slime = first * second;
             min_heap.push(Reverse(new_slime));
-            sum = new_slime * sum % DIVISOR;
+            sum = ((new_slime % DIVISOR) * sum) % DIVISOR;
         }
         writeln!(output, "{}", sum).unwrap();
     }
