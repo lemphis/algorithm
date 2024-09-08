518741bbb82dc20ad68f8bff78b8b55b75f0056b
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-09-08 13:35:20 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.rs
@@ -0,0 +1,45 @@
+use std::{fmt::Write, io};
+
+fn main() {
+    let buf = io::read_to_string(io::stdin()).unwrap();
+    let mut input = buf.split_whitespace();
+    let vowels = ['a', 'e', 'i', 'o', 'u'];
+    let mut output = String::new();
+    loop {
+        let password = input.next().unwrap();
+        if password == "end" {
+            break;
+        }
+        let mut is_possible = true;
+        let chars = password.chars().collect::<Vec<_>>();
+        if chars.iter().all(|c| !vowels.contains(c)) {
+            is_possible = false;
+        }
+        let mut consonant_pos = -1;
+        let mut vowel_pos = -1;
+        for (i, c) in chars.iter().enumerate() {
+            if vowels.contains(c) {
+                vowel_pos = i as i32;
+            } else {
+                consonant_pos = i as i32;
+            }
+            if (consonant_pos - vowel_pos).abs() == 3 {
+                is_possible = false;
+                break;
+            }
+        }
+        if chars
+            .windows(2)
+            .any(|w| w[0] == w[1] && w[0] != 'e' && w[0] != 'o')
+        {
+            is_possible = false;
+        }
+        writeln!(
+            output,
+            "<{password}> is {}acceptable.",
+            if is_possible { "" } else { "not " }
+        )
+        .unwrap();
+    }
+    print!("{output}");
+}
