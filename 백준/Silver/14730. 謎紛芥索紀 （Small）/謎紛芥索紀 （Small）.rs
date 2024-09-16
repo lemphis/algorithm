a56d910c52d9dcbff9a683d6d63e7a3fe0918c87
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-09-16 14:41:48 +0900

    [Silver V] Title: 謎紛芥索紀 (Small), Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/14730. 謎紛芥索紀 （Small）/謎紛芥索紀 （Small）.rs
+++ 백준/Silver/14730. 謎紛芥索紀 （Small）/謎紛芥索紀 （Small）.rs
@@ -4,10 +4,5 @@ fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
     let mut input = buf.split_whitespace().flat_map(str::parse::<i32>);
     let n = input.next().unwrap() as usize;
-    let mut nums = vec![0; n];
-    for num in nums.iter_mut() {
-        let (c, k) = (input.next().unwrap(), input.next().unwrap());
-        *num = c * k;
-    }
-    println!("{}", nums.iter().fold(0, |acc, cur| acc + cur));
+    println!("{}", (0..n).map(|_| input.next().unwrap() * input.next().unwrap()).fold(0, |acc, cur| acc + cur));
 }
