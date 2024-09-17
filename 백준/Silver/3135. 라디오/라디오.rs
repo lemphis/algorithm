34764a45985c80b1034207c45fab3dc36e6ea21b
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-09-17 21:37:41 +0900

    [Silver V] Title: 라디오, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Silver/3135. 라디오/라디오.rs
+++ 백준/Silver/3135. 라디오/라디오.rs
@@ -5,6 +5,5 @@ fn main() {
     let mut input = buf.split_whitespace().flat_map(str::parse::<i32>);
     let (a, b) = (input.next().unwrap(), input.next().unwrap());
     let min_diff = input.skip(1).collect::<Vec<_>>().iter().map(|it| (b - it).abs()).min().unwrap();
-    let freq_diff = (b - a).abs();
-    print!("{}", if min_diff < freq_diff {min_diff + 1} else {freq_diff});
+    print!("{}", (b - a).abs().min(min_diff + 1));
 }
