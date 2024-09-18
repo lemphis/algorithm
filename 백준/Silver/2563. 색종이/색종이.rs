b0b8e0c16fbf79d5a205e6d6abcbe3a1c45a69f7
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-09-18 13:05:45 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.rs
+++ 백준/Silver/2563. 색종이/색종이.rs
@@ -2,28 +2,17 @@ use std::io;
 
 fn main() {
     let buf = io::read_to_string(io::stdin()).unwrap();
-    let input = buf.split_whitespace().flat_map(str::parse::<usize>);
-    let mut is_paper = [[false; 100]; 100];
-    let papers = input
-        .skip(1)
-        .collect::<Vec<_>>()
-        .chunks(2)
-        .map(|c| (c[0], c[1]))
-        .collect::<Vec<_>>();
-    for &(x, y) in papers.iter() {
+    let mut input = buf.split_whitespace().flat_map(str::parse::<usize>);
+    let mut is_paper = [[0; 100]; 100];
+    for _ in 0..input.next().unwrap() {
+        let (x, y) = (input.next().unwrap(), input.next().unwrap());
         for r in is_paper.iter_mut().skip(y).take(10) {
-            for item in r.iter_mut().skip(x).take(10) {
-                *item = true;
-            }
+            r[x..x + 10].fill(1);
         }
     }
     let mut count = 0;
     for r in is_paper.iter() {
-        for item in r.iter() {
-            if *item {
-                count += 1;
-            }
-        }
+        count += r.iter().sum::<usize>();
     }
     print!("{count}");
 }
