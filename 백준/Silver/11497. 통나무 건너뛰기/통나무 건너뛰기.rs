0a6fbdbb33fcbd9c6cc059ee238b392d024a3efc
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-09-25 08:56:05 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 16 ms, Memory: 15532 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.rs
@@ -0,0 +1,31 @@
+use std::{
+    collections::VecDeque,
+    fmt::Write,
+    io::{self, stdin},
+};
+
+fn main() {
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let t = input.next().unwrap();
+    let mut output = String::new();
+    for _ in 0..t {
+        let n = input.next().unwrap();
+        let mut arr = input.by_ref().take(n).collect::<Vec<_>>();
+        arr.sort_unstable();
+        let mut logs = VecDeque::with_capacity(n);
+        for (i, &log) in arr.iter().enumerate() {
+            if i & 1 == 0 {
+                logs.push_front(log);
+            } else {
+                logs.push_back(log);
+            }
+        }
+        let mut max_diff = 0;
+        for i in 0..n - 1 {
+            max_diff = max_diff.max(logs[i + 1].abs_diff(logs[i]));
+        }
+        writeln!(output, "{}", max_diff).unwrap();
+    }
+    print!("{output}");
+}
