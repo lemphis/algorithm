e27e1de6ef5735268245a782737a27081d7b461b
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-10-25 12:55:34 +0900

    [Silver V] Title: 아시아 정보올림피아드, Time: 0 ms, Memory: 13212 KB -BaekjoonHub

--- 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.rs
+++ 백준/Silver/2535. 아시아 정보올림피아드/아시아 정보올림피아드.rs
@@ -3,31 +3,25 @@ use std::io::{self, stdin};
 fn main() {
     let buf = io::read_to_string(stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
-    let n = input.next().unwrap();
+    let mut input = || input.next().unwrap();
+    let n = input();
     let mut players = (0..n)
-        .map(|_| {
-            (
-                input.next().unwrap(),
-                input.next().unwrap(),
-                input.next().unwrap(),
-            )
-        })
+        .map(|_| (input(), input(), input()))
         .collect::<Vec<_>>();
     players.sort_by(|&a, &b| b.2.cmp(&a.2));
     let mut output = String::new();
-    players
-        .iter()
-        .take(2)
-        .for_each(|&(country, player, _)| output.push_str(&format!("{} {}\n", country, player)));
-    if players[0].0 == players[1].0 {
-        for &(country, player, _) in players.iter().skip(2) {
-            if country != players[0].0 {
-                output.push_str(&format!("{} {}", country, player));
-                break;
-            }
+    let mut count = vec![0; n + 1];
+    let mut total_count = 0;
+    for &(country, player, _) in players.iter() {
+        if count[country] == 2 {
+            continue;
         }
-    } else {
-        output.push_str(&format!("{} {}", players[2].0, players[2].1))
+        output.push_str(&format!("{} {}\n", country, player));
+        count[country] += 1;
+        total_count += 1;
+        if total_count == 3 {
+            break;
+        }
     }
     print!("{output}");
 }
