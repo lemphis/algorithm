367898183cdb05f21abd38629a50d6ec062eaf4d
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-10-26 11:48:48 +0900

    [Silver IV] Title: 온라인 판매, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.rs
@@ -0,0 +1,22 @@
+use std::io::{self, stdin};
+
+fn main() {
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut input = || input.next().unwrap();
+    let (n, m) = (input(), input());
+    let mut prices = (0..m).map(|_| input()).collect::<Vec<_>>();
+    prices.sort_unstable_by(|a, b| b.cmp(a));
+    let (mut min_price, mut max_total_price) = (0, 0);
+    for (i, &price) in prices.iter().enumerate() {
+        if i + 1 > n {
+            break;
+        }
+        let total_price = price * (i + 1);
+        if total_price > max_total_price {
+            min_price = price;
+            max_total_price = total_price;
+        }
+    }
+    print!("{} {}", min_price, max_total_price);
+}
