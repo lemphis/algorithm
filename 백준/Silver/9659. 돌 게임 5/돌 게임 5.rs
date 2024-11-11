3e4ad3dc91e5f4f077c5b761ea38bfc494fda0b7
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-11-11 21:29:19 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.rs
@@ -0,0 +1,8 @@
+use std::io::{self, stdin};
+
+fn main() {
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let n = input.next().unwrap();
+    print!("{}", if n & 1 == 0 { "CY" } else { "SK" });
+}
