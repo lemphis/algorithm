e72b25f1ad625fbbd58bad43acbf1d677fef3295
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-11-15 09:46:00 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.rs
@@ -0,0 +1,8 @@
+use std::io::{self, stdin};
+
+fn main() {
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let n = input.next().unwrap();
+    print!("{}", (n as f64).sqrt() as usize);
+}
