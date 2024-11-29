6ef6a389b16ac12fbe3b5534d3d7e5a36a0f86df
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-11-29 13:33:54 +0900

    [Silver V] Title: 자료구조는 정말 최고야, Time: 8 ms, Memory: 15256 KB -BaekjoonHub

--- 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.rs
+++ 백준/Silver/23253. 자료구조는 정말 최고야/자료구조는 정말 최고야.rs
@@ -1,18 +1,15 @@
-use std::{
-    io::{self, stdin},
-    usize,
-};
+use std::io::{self, stdin};
 
 fn main() {
     let buf = io::read_to_string(stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let mut f = || input.next().unwrap();
-    let (_, m) = (f(), f());
+    let (n, m) = (f(), f());
     for _ in 0..m {
         let k = f();
-        let mut last = usize::MAX;
+        let mut last = n;
         for _ in 0..k {
-            let num = f() - 1;
+            let num = f();
             if last < num {
                 print!("No");
                 return;
