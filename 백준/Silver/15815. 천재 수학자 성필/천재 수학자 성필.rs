a924613b49199a6fc8fa2424b405485afb8cb70f
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-12-10 23:39:16 +0900

    [Silver III] Title: 천재 수학자 성필, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.rs
+++ 백준/Silver/15815. 천재 수학자 성필/천재 수학자 성필.rs
@@ -3,18 +3,19 @@ use std::io::{self, stdin};
 fn main() {
     let buf = io::read_to_string(stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace();
-    let formula = input.next().unwrap().chars();
-    let mut stack = Vec::with_capacity(formula.clone().count());
-    for c in formula {
+    let formula = input.next().unwrap();
+    let mut stack = Vec::with_capacity(formula.len());
+    for c in formula.chars() {
         if c.is_ascii_digit() {
             stack.push(c.to_digit(10).unwrap() as i32);
         } else {
-            let (right, left) = (stack.pop().unwrap(), stack.pop().unwrap());
+            let mut p = || stack.pop().unwrap();
+            let (r, l) = (p(), p());
             stack.push(match c {
-                '*' => left * right,
-                '/' => left / right,
-                '+' => left + right,
-                '-' => left - right,
+                '*' => l * r,
+                '/' => l / r,
+                '+' => l + r,
+                '-' => l - r,
                 _ => unreachable!(),
             });
         }
