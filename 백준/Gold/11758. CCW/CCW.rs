a77ba00dd63d5f1097fc0bdd4fe088e882671e10
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2024-12-18 13:10:16 +0900

    [Gold V] Title: CCW, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Gold/11758. CCW/CCW.rs
+++ 백준/Gold/11758. CCW/CCW.rs
@@ -5,13 +5,8 @@ fn main() {
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
     let mut f = || input.next().unwrap();
     let (x1, y1, x2, y2, x3, y3) = (f(), f(), f(), f(), f(), f());
-    let result = (x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3);
     print!(
         "{}",
-        match result {
-            result if result >= 1 => 1,
-            result if result <= -1 => -1,
-            _ => 0,
-        }
+        ((x1 * y2 + x2 * y3 + x3 * y1) - (x2 * y1 + x3 * y2 + x1 * y3)).signum()
     )
 }
