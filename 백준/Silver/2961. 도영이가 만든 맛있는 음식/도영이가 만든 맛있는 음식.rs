6bfcdefde0e003e029aeb084cb5e9db6814a93bc
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-01-07 12:57:38 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.rs
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.rs
@@ -9,29 +9,18 @@ fn main() {
     let mut f = || input.next().unwrap();
     let n = f();
     let ingredients = (0..n).map(|_| (f(), f())).collect::<Vec<_>>();
-    let mut visited = vec![false; n];
     let mut ans = usize::MAX;
-    dfs(&ingredients, &mut visited, &mut ans, (1, 0), 0);
+    dfs(&ingredients, &mut ans, (1, 0), 0);
     print!("{ans}");
 }
 
-fn dfs(
-    ingredients: &[(usize, usize)],
-    visited: &mut [bool],
-    ans: &mut usize,
-    sb: (usize, usize),
-    depth: usize,
-) {
-    if depth == ingredients.len() {
+fn dfs(ingredients: &[(usize, usize)], ans: &mut usize, sb: (usize, usize), start: usize) {
+    if start == ingredients.len() {
         return;
     }
-    for i in 0..ingredients.len() {
-        if !visited[i] {
-            visited[i] = true;
-            let next_sb = (sb.0 * ingredients[i].0, sb.1 + ingredients[i].1);
-            *ans = *ans.min(&mut next_sb.0.abs_diff(next_sb.1));
-            dfs(ingredients, visited, ans, next_sb, depth + 1);
-            visited[i] = false;
-        }
+    for i in start..ingredients.len() {
+        let next_sb = (sb.0 * ingredients[i].0, sb.1 + ingredients[i].1);
+        *ans = (*ans).min(next_sb.0.abs_diff(next_sb.1));
+        dfs(ingredients, ans, next_sb, i + 1);
     }
 }
