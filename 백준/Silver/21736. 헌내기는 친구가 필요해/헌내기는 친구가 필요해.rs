2a41f5dc7c30657e2339bff56eb606981542be85
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-01-08 13:15:28 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 8 ms, Memory: 15680 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.rs
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.rs
@@ -11,7 +11,7 @@ fn main() {
     let mut input = buf.split_ascii_whitespace();
     let mut f = || input.next().unwrap();
     let (n, m) = (f().parse::<usize>().unwrap(), f().parse::<usize>().unwrap());
-    let (mut map, mut visited) = (vec![vec!['O'; m]; n], vec![vec![false; m]; n]);
+    let mut map = vec![vec!['O'; m]; n];
     let mut doyeon_pos = (0, 0);
     for r in 0..n {
         for (c, ch) in f().chars().enumerate() {
@@ -21,24 +21,22 @@ fn main() {
             }
         }
     }
-    let ans = bfs(&map, &mut visited, doyeon_pos);
-    if ans > 0 {
-        print!("{ans}")
-    } else {
-        print!("TT");
-    }
+    let ans = bfs(&mut map, doyeon_pos);
+    print!(
+        "{}",
+        if ans > 0 {
+            ans.to_string()
+        } else {
+            String::from("TT")
+        }
+    );
 }
 
-fn bfs(map: &[Vec<char>], visited: &mut [Vec<bool>], start: (usize, usize)) -> usize {
-    let mut q = VecDeque::new();
-    q.push_back(start);
-    visited[start.0][start.1] = true;
+fn bfs(map: &mut [Vec<char>], start: (usize, usize)) -> usize {
+    let mut q = VecDeque::from([start]);
+    map[start.0][start.1] = 'X';
     let mut ans = 0;
-    while !q.is_empty() {
-        let (r, c) = q.pop_front().unwrap();
-        if map[r][c] == 'P' {
-            ans += 1;
-        }
+    while let Some((r, c)) = q.pop_front() {
         for i in 0..4 {
             let (next_r, next_c) = (r as i32 + DR[i], c as i32 + DC[i]);
             if next_r >= 0
@@ -46,10 +44,12 @@ fn bfs(map: &[Vec<char>], visited: &mut [Vec<bool>], start: (usize, usize)) -> u
                 && next_c >= 0
                 && next_c < map[r].len() as i32
                 && map[next_r as usize][next_c as usize] != 'X'
-                && !visited[next_r as usize][next_c as usize]
             {
+                if map[next_r as usize][next_c as usize] == 'P' {
+                    ans += 1;
+                }
+                map[next_r as usize][next_c as usize] = 'X';
                 q.push_back((next_r as usize, next_c as usize));
-                visited[next_r as usize][next_c as usize] = true;
             }
         }
     }
