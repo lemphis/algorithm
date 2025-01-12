9745c71b96d060e0152e7034a8947e13f5365162
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-01-12 18:04:43 +0900

    [Silver III] Title: 마라톤 1, Time: 8 ms, Memory: 15412 KB -BaekjoonHub

--- 백준/Silver/10655. 마라톤 1/마라톤 1.rs
+++ 백준/Silver/10655. 마라톤 1/마라톤 1.rs
@@ -3,27 +3,20 @@ use std::io::{self, stdin};
 fn main() {
     let buf = io::read_to_string(stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
-    let mut next = || input.next().unwrap();
-
-    let n = next() as usize;
-    let poses = (0..n).map(|_| (next(), next())).collect::<Vec<_>>();
-
-    let mut total_distance = 0;
-    let mut diffs = vec![0; n - 1];
-
-    for i in 0..n - 1 {
-        let dist = (poses[i].0 - poses[i + 1].0).abs() + (poses[i].1 - poses[i + 1].1).abs();
-        diffs[i] = dist;
-        total_distance += dist;
+    let mut f = || input.next().unwrap();
+    let n = f() as usize;
+    let poses = (0..n).map(|_| (f(), f())).collect::<Vec<_>>();
+    let diffs = (0..n - 1)
+        .map(|i| (poses[i].0 - poses[i + 1].0).abs() + (poses[i].1 - poses[i + 1].1).abs())
+        .collect::<Vec<_>>();
+    let diffs_sum = diffs.iter().sum::<i32>();
+    let mut diffs_min_sum = diffs_sum;
+    for (i, &(x, y)) in poses.iter().take(n - 2).enumerate() {
+        diffs_min_sum = diffs_min_sum.min(
+            diffs_sum - diffs[i] - diffs[i + 1]
+                + (poses[i + 2].0 - x).abs()
+                + (poses[i + 2].1 - y).abs(),
+        );
     }
-
-    let mut min_distance = total_distance;
-
-    for i in 0..n - 2 {
-        let skip_original = diffs[i] + diffs[i + 1];
-        let skip_new = (poses[i].0 - poses[i + 2].0).abs() + (poses[i].1 - poses[i + 2].1).abs();
-        min_distance = min_distance.min(total_distance - skip_original + skip_new);
-    }
-
-    println!("{}", min_distance);
+    print!("{diffs_min_sum}");
 }
