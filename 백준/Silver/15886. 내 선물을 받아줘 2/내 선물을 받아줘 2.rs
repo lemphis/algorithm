7b57de64968664f6196a75f197c1c88b497e7cd9
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-01-13 20:33:49 +0900

    [Silver III] Title: 내 선물을 받아줘 2, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.rs
+++ 백준/Silver/15886. 내 선물을 받아줘 2/내 선물을 받아줘 2.rs
@@ -5,12 +5,11 @@ fn main() {
     let mut input = buf.split_ascii_whitespace();
     let mut f = || input.next().unwrap();
     let (_, s) = (f().parse::<usize>().unwrap(), f());
-    let mut count = 0;
-    let chars = s.chars().collect::<Vec<_>>();
-    for (i, &c) in chars.iter().enumerate().skip(1) {
-        if c == 'W' && chars[i - 1] == 'E' {
-            count += 1;
-        }
-    }
+    let count = s
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(2)
+        .filter(|w| **w == ['E', 'W'])
+        .count();
     print!("{count}");
 }
