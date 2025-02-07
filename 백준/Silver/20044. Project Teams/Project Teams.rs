bdd20d755cd2051a99c01169b3fee7e7dc9bc753
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-02-07 19:28:51 +0900

    [Silver IV] Title: Project Teams, Time: 4 ms, Memory: 13356 KB -BaekjoonHub

--- 백준/Silver/20044. Project Teams/Project Teams.rs
+++ 백준/Silver/20044. Project Teams/Project Teams.rs
@@ -7,14 +7,11 @@ fn main() {
     let n = f();
     let mut powers: Vec<usize> = (0..n * 2).map(|_| f()).collect();
     powers.sort_unstable();
-
-    let mut min_sum = usize::MAX;
-    for i in 0..n {
-        let sum = powers[i] + powers[n * 2 - 1 - i];
-        if sum < min_sum {
-            min_sum = sum;
-        }
-    }
-
-    print!("{min_sum}");
+    print!(
+        "{}",
+        (0..n)
+            .map(|i| powers[i] + powers[n * 2 - 1 - i])
+            .min()
+            .unwrap()
+    );
 }
