9bdd2780fcff4b9a5dec9eec2b07c065f2b1b44a
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-02-09 12:09:41 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 13208 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.rs
@@ -0,0 +1,21 @@
+use std::io::{self, stdin};
+
+const DIV: u32 = 1_234_567_891;
+
+fn main() {
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace();
+    let mut f = || input.next().unwrap();
+    let (_, s) = (f(), f());
+    let chars: Vec<char> = s.chars().collect();
+    let mut ans = 0;
+    for (i, c) in chars.iter().enumerate() {
+        let mut sum = (*c as u8 - b'a' + 1) as u32;
+        for _ in 1..=i {
+            sum = (sum * 31_u32) % DIV;
+        }
+        dbg!(sum);
+        ans = (ans + sum) % DIV;
+    }
+    print!("{ans}");
+}
