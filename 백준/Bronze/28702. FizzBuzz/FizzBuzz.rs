6c52d6b3595bf8978330a49f5e16439bca489e92
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-02-09 12:49:00 +0900

    [Bronze I] Title: FizzBuzz, Time: 0 ms, Memory: 13208 KB -BaekjoonHub

--- 백준/Bronze/28702. FizzBuzz/FizzBuzz.rs
+++ 백준/Bronze/28702. FizzBuzz/FizzBuzz.rs
@@ -5,25 +5,24 @@ fn main() {
     let mut input = buf.split_ascii_whitespace();
     let mut f = || input.next().unwrap();
     let (a, b, c) = (f(), f(), f());
-    if let Ok(n) = a.parse::<usize>() {
-        print!("{}", fizz_buzz(n + 3));
-        return;
-    }
-    if let Ok(n) = b.parse::<usize>() {
-        print!("{}", fizz_buzz(n + 2));
-        return;
-    }
-    if let Ok(n) = c.parse::<usize>() {
-        print!("{}", fizz_buzz(n + 1));
-        return;
-    }
+    let next_index = if let Ok(n) = c.parse::<usize>() {
+        n + 1
+    } else if let Ok(n) = b.parse::<usize>() {
+        n + 2
+    } else if let Ok(n) = a.parse::<usize>() {
+        n + 3
+    } else {
+        unreachable!();
+    };
+
+    print!("{}", fizz_buzz(next_index));
 }
 
 fn fizz_buzz(i: usize) -> String {
-    match i {
-        i if i % 3 == 0 && i % 5 != 0 => String::from("Fizz"),
-        i if i % 3 != 0 && i % 5 == 0 => String::from("Buzz"),
-        i if i % 3 == 0 && i % 5 == 0 => String::from("FizzBuzz"),
-        i => i.to_string(),
+    match (i % 3 == 0, i % 5 == 0) {
+        (true, true) => "FizzBuzz".to_string(),
+        (true, false) => "Fizz".to_string(),
+        (false, true) => "Buzz".to_string(),
+        (false, false) => i.to_string(),
     }
 }
