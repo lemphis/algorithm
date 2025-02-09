a05179b9080283adb16f6bc174a12abc6c8d578e
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-02-09 10:46:32 +0900

    [Bronze III] Title: 웰컴 키트, Time: 0 ms, Memory: 13204 KB -BaekjoonHub

--- 백준/Bronze/30802. 웰컴 키트/웰컴 키트.rs
+++ 백준/Bronze/30802. 웰컴 키트/웰컴 키트.rs
@@ -2,19 +2,15 @@ use std::io::{self, stdin};
 
 fn main() {
     let buf = io::read_to_string(stdin()).unwrap();
-    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<i32>);
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let mut f = || input.next().unwrap();
     let n = f();
     let size_count = [f(), f(), f(), f(), f(), f()];
     let (t, p) = (f(), f());
-
-    let mut t_count = 0;
-    for c in size_count {
-        t_count += c / t;
-        if c % t > 0 {
-            t_count += 1;
-        }
-    }
-
-    print!("{}\n{} {}", t_count, n / p, n % p);
+    print!(
+        "{}\n{} {}",
+        size_count.iter().map(|c| c.div_ceil(t)).sum::<usize>(),
+        n / p,
+        n % p
+    );
 }
