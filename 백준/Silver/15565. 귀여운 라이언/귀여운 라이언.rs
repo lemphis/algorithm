4f024df9c8ffe2b69d6dec28bfded55315dad92c
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-02-14 16:09:00 +0900

    [Silver I] Title: 귀여운 라이언, Time: 16 ms, Memory: 17308 KB -BaekjoonHub

--- 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.rs
+++ 백준/Silver/15565. 귀여운 라이언/귀여운 라이언.rs
@@ -1,30 +1,22 @@
-use std::{
-    io::{self, stdin},
-    usize,
-};
+use std::io::{self, stdin};
 
 fn main() {
     let buf = io::read_to_string(stdin()).unwrap();
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let mut f = || input.next().unwrap();
     let (n, k) = (f(), f());
-    let is_ryan: Vec<bool> = (0..n).map(|_| f() == 1).collect();
-
-    let mut ans = usize::MAX;
-    let (mut l, mut ryan_count) = (0, 0);
-    for r in 0..n {
-        if is_ryan[r] {
-            ryan_count += 1;
-        }
-        while ryan_count >= k {
-            if ryan_count == k {
-                ans = ans.min(r - l + 1);
-            }
-            if is_ryan[l] {
-                ryan_count -= 1;
-            }
-            l += 1;
+    let mut poses = Vec::new();
+    for i in 0..n {
+        if f() == 1 {
+            poses.push(i as i32);
         }
     }
-    print!("{}", if ans == usize::MAX { -1 } else { ans as i32 });
+    print!(
+        "{}",
+        poses
+            .windows(k)
+            .map(|w| w.last().unwrap() - w.first().unwrap() + 1)
+            .min()
+            .unwrap_or(-1)
+    );
 }
