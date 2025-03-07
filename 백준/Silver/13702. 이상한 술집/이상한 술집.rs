0b9e312102845ec4e559ddd068134432ff007401
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-03-07 11:37:34 +0900

    [Silver II] Title: 이상한 술집, Time: 4 ms, Memory: 13336 KB -BaekjoonHub

---
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.rs
@@ -0,0 +1,26 @@
+use std::io::{self, stdin};
+
+fn main() {
+    let buf = io::read_to_string(stdin()).unwrap();
+    let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
+    let mut f = || input.next().unwrap();
+    let (n, k) = (f(), f());
+    let kettles: Vec<usize> = (0..n).map(|_| f()).collect();
+    let (mut l, mut r) = (0, i32::MAX as usize);
+    while l + 1 < r {
+        let mid = (l + r) / 2;
+        match check(&kettles, k, mid) {
+            true => {
+                l = mid;
+            }
+            false => {
+                r = mid;
+            }
+        }
+    }
+    print!("{l}");
+}
+
+fn check(kettles: &[usize], k: usize, mid: usize) -> bool {
+    kettles.iter().map(|ml| ml / mid).sum::<usize>() >= k
+}
