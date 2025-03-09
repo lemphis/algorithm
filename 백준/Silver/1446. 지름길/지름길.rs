44a9328edba5e872e886ccfb010293b68d57670a
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-03-09 20:41:46 +0900

    [Silver I] Title: 지름길, Time: 0 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1446. 지름길/지름길.rs
+++ 백준/Silver/1446. 지름길/지름길.rs
@@ -1,8 +1,4 @@
-use std::{
-    cmp::Reverse,
-    collections::{hash_map, BinaryHeap, HashMap},
-    io::{self, BufReader, Read},
-};
+use std::io::{self, BufReader, Read};
 
 fn main() {
     let mut buf = String::with_capacity(1024 * 1024);
@@ -12,49 +8,17 @@ fn main() {
     let mut input = buf.split_ascii_whitespace().flat_map(str::parse::<usize>);
     let mut f = || input.next().unwrap();
     let (n, d) = (f(), f());
-    let mut adj = vec![HashMap::new(); 10_001];
-    for _ in 0..n {
-        let (s, e, w) = (f(), f(), f());
-        if e > d {
-            continue;
-        }
-        if e - s <= w {
-            continue;
-        }
-        match adj[s].entry(e) {
-            hash_map::Entry::Vacant(entry) => {
-                entry.insert(w);
-            }
-            hash_map::Entry::Occupied(mut entry) => {
-                *entry.get_mut() = (*entry.get()).min(w);
+    let shortcuts: Vec<_> = (0..n).map(|_| (f(), f(), f())).collect();
+    let mut dp = vec![0; d + 1];
+    for i in 1..=d {
+        let mut min_cost = dp[i - 1] + 1;
+        for &(s, e, w) in shortcuts.iter() {
+            if i == e {
+                min_cost = min_cost.min(dp[s] + w);
             }
         }
+        dp[i] = min_cost;
     }
 
-    print!("{}", dijkstra(&adj, d));
-}
-
-fn dijkstra(adj: &[HashMap<usize, usize>], d: usize) -> usize {
-    let mut dist = vec![usize::MAX; adj.len()];
-    dist[0] = 0;
-
-    let mut pq = BinaryHeap::new();
-    pq.push((Reverse(0), 0));
-
-    while let Some((Reverse(cost), pos)) = pq.pop() {
-        if pos == d {
-            return cost;
-        }
-        for (next, next_cost) in adj[pos].iter() {
-            let min = dist[*next].min(dist[pos] + *next_cost);
-            pq.push((Reverse(min), *next));
-            dist[*next] = min;
-        }
-
-        let min = dist[pos + 1].min(dist[pos] + 1);
-        pq.push((Reverse(min), pos + 1));
-        dist[pos + 1] = min;
-    }
-
-    unreachable!()
+    print!("{}", dp[d]);
 }
