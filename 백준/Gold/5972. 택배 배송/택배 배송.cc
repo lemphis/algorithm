c3c3b2b83c91932ccdfd5c919376e6fe0dc6c0b9
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-03-22 17:38:23 +0900

    [Gold V] Title: 택배 배송, Time: 28 ms, Memory: 8420 KB -BaekjoonHub

---
+++ 백준/Gold/5972. 택배 배송/택배 배송.cc
@@ -0,0 +1,51 @@
+#include <functional>
+#include <iostream>
+#include <queue>
+
+#define fastio cin.tie(0)->sync_with_stdio(0)
+#define endl '\n'
+
+using namespace std;
+
+using pii = pair<int, int>;
+
+vector<int> dijkstra(vector<vector<pii>> adj);
+
+int main() {
+    fastio;
+    int N, M;
+    cin >> N >> M;
+    vector<vector<pii>> adj(N);
+    for (int i = 0; i < M; ++i) {
+        int A, B, C;
+        cin >> A >> B >> C;
+        adj[A - 1].push_back({B - 1, C});
+        adj[B - 1].push_back({A - 1, C});
+    }
+
+    cout << dijkstra(adj)[N - 1];
+
+    return 0;
+}
+
+vector<int> dijkstra(vector<vector<pii>> adj) {
+    vector<int> dist(adj.size(), ~(1 << 31));
+    dist[0] = 0;
+
+    priority_queue<pii, vector<pii>, greater<pii>> pq;
+    pq.push({0, 0});
+    while (!pq.empty()) {
+        pii top = pq.top();
+        pq.pop();
+        int d = top.first;
+        int node = top.second;
+        for (int i = 0; i < adj[node].size(); ++i) {
+            if (dist[adj[node][i].first] > d + adj[node][i].second) {
+                dist[adj[node][i].first] = d + adj[node][i].second;
+                pq.push({dist[adj[node][i].first], adj[node][i].first});
+            }
+        }
+    }
+
+    return dist;
+}
