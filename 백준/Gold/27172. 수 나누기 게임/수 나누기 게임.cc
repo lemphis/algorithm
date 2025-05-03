f7626762c4452f1d31c89aa26671cca00ecd31b3
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-05-03 12:35:52 +0900

    [Gold IV] Title: 수 나누기 게임, Time: 40 ms, Memory: 6976 KB -BaekjoonHub

--- 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
+++ 백준/Gold/27172. 수 나누기 게임/수 나누기 게임.cc
@@ -1,6 +1,5 @@
 #include <cstring>
 #include <iostream>
-#include <sstream>
 #include <vector>
 
 #define fastio cin.tie(0)->sync_with_stdio(0)
@@ -37,12 +36,13 @@ int main() {
         }
     }
 
-    ostringstream output;
+    string output;
+    output.reserve(N * 4);
     for (int i = 0; i < N; ++i) {
-        output << scores[i] << ' ';
+        output += to_string(scores[i]);
+        output += ' ';
     }
-
-    cout << output.str();
+    cout << output;
 
     return 0;
 }
