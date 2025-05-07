b2ac938743913446d73965745c422530b19ae93b
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-05-08 07:32:16 +0900

    [Silver V] Title: 알록달록 앵무새, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.cc
+++ 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.cc
@@ -1,6 +1,5 @@
-#include <algorithm>
 #include <iostream>
-#include <unordered_set>
+#include <set>
 
 #define fastio cin.tie(0)->sync_with_stdio(0)
 #define endl '\n'
@@ -10,27 +9,20 @@ using namespace std;
 int main() {
     fastio;
 
-    unordered_set<string> colors;
+    set<string> colors;
     for (int i = 0; i < 4; ++i) {
         string color;
         cin >> color;
         colors.insert(color);
     }
 
-    vector<string> combinations;
-    for (const string& first : colors) {
-        for (const string& second : colors) {
-            combinations.push_back(first + ' ' + second);
-        }
-    }
-
-    sort(combinations.begin(), combinations.end());
-
     string output;
     output.reserve(20 * 16 * 2);
-    for (string& s : combinations) {
-        output += s;
-        output += '\n';
+    for (const string& first : colors) {
+        for (const string& second : colors) {
+            output += first + ' ' + second;
+            output += '\n';
+        }
     }
 
     cout << output;
