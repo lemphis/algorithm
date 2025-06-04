0ae70cbf28ef33b0dd0fbf87f6c2d496cd9586f0
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-06-04 13:15:22 +0900

    [Bronze I] Title: 컵홀더, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2810. 컵홀더/컵홀더.py
+++ 백준/Bronze/2810. 컵홀더/컵홀더.py
@@ -1,4 +1,4 @@
-N = input()
+N = int(input())
 seats = input()
 s_count, l_count = 0, 0
 for seat in seats:
@@ -7,9 +7,4 @@ for seat in seats:
     else:
         l_count += 1
 
-if l_count == 0:
-    print(N)
-elif s_count == 0:
-    print(N - 1)
-else:
-    print(s_count + (l_count // 2) + 1)
+print(N if l_count == 0 else s_count + (l_count // 2) + 1)
