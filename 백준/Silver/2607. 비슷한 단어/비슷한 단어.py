85334407216ae66504986a327ce5ee4d60d3f1d1
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-06-26 12:41:03 +0900

    [Silver II] Title: 비슷한 단어, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -1,31 +1,18 @@
 n = int(input())
 origin_word = input()
-origin_char_count = [0] * 26
+origin_count = [0] * 26
 for c in origin_word:
-    origin_char_count[ord(c) - ord("A")] += 1
+    origin_count[ord(c) - ord("A")] += 1
 
-words = [input() for _ in range(n - 1)]
 ans = 0
-for word in words:
-    if abs(len(origin_word) - len(word)) > 1:
-        continue
+for _ in range(n - 1):
+    word = input()
     char_count = [0] * 26
     for c in word:
         char_count[ord(c) - ord("A")] += 1
-    is_similar = True
-    diff_count = 0
-    for i in range(26):
-        diff = abs(origin_char_count[i] - char_count[i])
-        if diff > 1:
-            is_similar = False
-            break
-        elif diff == 1:
-            diff_count += 1
-        if diff_count > 2:
-            is_similar = False
-            break
-    if is_similar:
+    plus = sum(max(origin_count[i] - char_count[i], 0) for i in range(26))
+    minus = sum(max(char_count[i] - origin_count[i], 0) for i in range(26))
+    if plus <= 1 and minus <= 1:
         ans += 1
-
 
 print(ans)
