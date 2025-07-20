0cbaca470c200c150c89149aa58289309ce07fd4
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-07-20 15:16:30 +0900

    [Silver IV] Title: 제 2회 IUPC는 잘 개최될 수 있을까?, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12788. 제 2회 IUPC는 잘 개최될 수 있을까？/제 2회 IUPC는 잘 개최될 수 있을까？.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+M, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+A.sort(reverse=True)
+total_pen_count = M * K
+
+idx = 0
+while total_pen_count > 0 and idx < N:
+    total_pen_count -= A[idx]
+    idx += 1
+
+print("STRESS" if idx == N and total_pen_count > 0 else idx)
