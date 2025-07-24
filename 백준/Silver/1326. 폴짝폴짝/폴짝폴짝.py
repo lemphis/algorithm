95d46ad6ba250063a995508145564a18b53ec7ea
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-07-24 12:15:41 +0900

    [Silver II] Title: 폴짝폴짝, Time: 72 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
+++ 백준/Silver/1326. 폴짝폴짝/폴짝폴짝.py
@@ -6,27 +6,26 @@ input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().split()))
 a, b = map(int, input().split())
+a -= 1
+b -= 1
 
-dq = deque([(a - 1, 0)])
+dq = deque([(a, 0)])
 visited = [False] * N
-visited[a - 1] = True
+visited[a] = True
 while dq:
-    now_pos, now_count = dq.popleft()
-    if now_pos == b - 1:
-        print(now_count)
-        exit()
+    pos, count = dq.popleft()
+    if pos == b:
+        print(count)
+        break
 
-    times = 1
-    while (next_pos := now_pos + (-times * nums[now_pos])) >= 0:
+    step = nums[pos]
+    for next_pos in range(pos + step, N, step):
         if not visited[next_pos]:
-            dq.append((next_pos, now_count + 1))
+            dq.append((next_pos, count + 1))
             visited[next_pos] = True
-        times += 1
-    times = 1
-    while (next_pos := now_pos + (times * nums[now_pos])) < N:
+    for next_pos in range(pos - step, -1, -step):
         if not visited[next_pos]:
-            dq.append((next_pos, now_count + 1))
+            dq.append((next_pos, count + 1))
             visited[next_pos] = True
-        times += 1
-
-print(-1)
+else:
+    print(-1)
