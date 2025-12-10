6324ad363c4a3406b3a91b5e781adde43301c80d
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2025-12-10 16:00:09 +0900

    [Gold V] Title: 캠프 준비, Time: 128 ms, Memory: 110960 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.py
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.py
@@ -2,28 +2,24 @@ import sys
 
 input = sys.stdin.readline
 
-
-def dfs(depth, candidate, levels):
-    global N, L, R, X, candidates
-    total = sum(candidate)
-    if total > R:
-        return
-
-    if depth == N:
-        if len(candidate) > 1 and total >= L and candidate[-1] - candidate[0] >= X:
-            candidates.append(candidate)
-        return
-
-    dfs(depth + 1, candidate, levels)
-    dfs(depth + 1, [*candidate, levels[depth]], levels)
-
-
 N, L, R, X = map(int, input().split())
-levels = list(map(int, input().split()))
+A = list(map(int, input().split()))
 
-levels.sort()
+ans = 0
+for mask in range(1 << N):
+    problems = []
+    for i in range(N):
+        if mask & (1 << i):
+            problems.append(A[i])
 
-candidates = []
-dfs(0, [], levels)
+    if len(problems) < 2:
+        continue
 
-print(len(candidates))
+    min_level = min(problems)
+    max_level = max(problems)
+    total_level = sum(problems)
+
+    if L <= total_level <= R and max_level - min_level >= X:
+        ans += 1
+
+print(ans)
