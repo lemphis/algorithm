c80f333569bf3920596d93a07c0fc1837eb8683b
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2026-01-06 23:59:42 +0900

    [Silver II] Title: 격자상의 경로, Time: 100 ms, Memory: 108384 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.py
@@ -1,34 +1,12 @@
 import sys
-from collections import deque
+import math
 
 input = sys.stdin.readline
 
 N, M, K = map(int, input().split())
 
-mid = (0, 0) if K == 0 else ((K - 1) // M, (K - 1) % M)
-
-dq = deque([(0, 0)])
-mid_count = 0
-while dq:
-    r, c = dq.popleft()
-    if (r, c) == mid:
-        mid_count += 1
-        continue
-    for dr, dc in [(0, 1), (1, 0)]:
-        nr, nc = r + dr, c + dc
-        if 0 <= nr < mid[0] + 1 and 0 <= nc < mid[1] + 1:
-            dq.append((nr, nc))
-
-end_count = 0
-dq = deque([mid])
-while dq:
-    r, c = dq.popleft()
-    if (r, c) == (N - 1, M - 1):
-        end_count += 1
-        continue
-    for dr, dc in [(0, 1), (1, 0)]:
-        nr, nc = r + dr, c + dc
-        if 0 <= nr < N and 0 <= nc < M:
-            dq.append((nr, nc))
-
-print(end_count if K == 0 else mid_count * end_count)
+if K == 0:
+    print(math.comb((N - 1) + (M - 1), N - 1))
+else:
+    r, c = (K - 1) // M, (K - 1) % M
+    print(math.comb(r + c, r) * math.comb((N - 1 - r) + (M - 1 - c), N - 1 - r))
