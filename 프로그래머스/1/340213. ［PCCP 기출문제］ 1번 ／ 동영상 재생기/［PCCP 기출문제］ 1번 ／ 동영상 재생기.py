1bba3c4ea5b8d97fee60cf6733a6b1247d249e52
Jaeho Jang <46966293+lemphis@users.noreply.github.com>
2026-07-05 16:17:40 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.02 ms, Memory: 11.8 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,27 +1,21 @@
 def solution(video_len, pos, op_start, op_end, commands):
-    now_s = to_second(pos)
-    op_start_s = to_second(op_start)
-    op_end_s = to_second(op_end)
-    end_s = to_second(video_len)
+    video_len_strs = video_len.split(":")
+    video_len_num = int(video_len_strs[0]) * 60 + int(video_len_strs[1])
+    pos_strs = pos.split(":")
+    time = int(pos_strs[0]) * 60 + int(pos_strs[1])
+    op_start_strs = op_start.split(":")
+    op_start_num = int(op_start_strs[0]) * 60 + int(op_start_strs[1])
+    op_end_strs = op_end.split(":")
+    op_end_num = int(op_end_strs[0]) * 60 + int(op_end_strs[1])
+
     for command in commands:
-        if op_start_s <= now_s < op_end_s:
-            now_s = op_end_s
+        if op_start_num <= time <= op_end_num:
+                time = op_end_num
         if command == "prev":
-            now_s -= 10
+            time = max(0, time - 10)
         else:
-            now_s += 10
-        if now_s < 0:
-            now_s = 0
-        if now_s > end_s:
-            now_s = end_s
-
-    if op_start_s <= now_s < op_end_s:
-        now_s = op_end_s
-    h = now_s // 60
-    m = now_s % 60
-    return f"{h:02d}:{m:02d}"
-
-
-def to_second(time):
-    h, m = map(int, time.split(":"))
-    return h * 60 + m
+            time = min(video_len_num, time + 10)
+        if op_start_num <= time <= op_end_num:
+                time = op_end_num
+                
+    return f"{(time // 60):02d}:{(time % 60):02d}"
